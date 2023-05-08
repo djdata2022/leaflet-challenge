5a559947de8d8da8446a8208e62f74a26a3a1e7e
djdata2022 <desijo@yahoo.com>
2023-05-07 20:07:29 -0400

    Final edit - addition of legend

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -3,15 +3,15 @@
 // Create the map object
 var myMap = L.map("map", {
   center: [
-    0, -40.71
+    20, 0
   ],
-  zoom:3, // Adjust zoom to see most of the points across the world
+  zoom: 2, // Adjust zoom to see most of the points across the world
   //layers: [street, earthquakes]
 });
 
 // Add the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 // Store our API endpoint as queryUrl - Past 7 Days M1.0+ Earthquakes
@@ -21,25 +21,24 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_we
 // Earthquakes with greater depth should appear darker in color -
 // Define function to determine color based on depth
 function chooseColor(depth) {
-  if (depth > 120) return "saddlebrown";
-  else if (depth > 100) return "sienna";
-  else if (depth > 80) return "chocolate";
-  else if (depth > 60) return "darksalmon";
-  else if (depth > 40) return "darkorange";
-  else if (depth > 20) return "orange";
-  else if (depth > 10) return "sandybrown";
-  else return "wheat";
+  if (depth > 300) return "darkred";
+  else if (depth > 250) return "brown";
+  else if (depth > 200) return "chocolate";
+  else if (depth > 150) return "darkorange";
+  else if (depth > 100) return "gold";
+  else if (depth > 50) return "khaki";
+  else return "lightyellow";
 }
 
 // Markers should reflect the magnitude of the earthquake by their size
 // Earthquakes with higher magnitudes should appear larger -
 // Define function to determine size of marker based on magnitude of earthquake
 function chooseRadius(magnitude) {
-  return magnitude*2;
+  return magnitude * 2;
 }
 
 // Use d3 to access data 
-d3.json(queryUrl).then(function(data) {
+d3.json(queryUrl).then(function (data) {
   console.log(data);
   let featuresArray = data.features;
 
@@ -53,13 +52,13 @@ d3.json(queryUrl).then(function(data) {
   // }
 
   //Use GeoJSON Leaflet function to plot and pointToLayer to add marker
-  
+
   L.geoJson(data, {
     // Include popups that provide additional info when marker is clicked
-    onEachFeature: function (feature, layer){
+    onEachFeature: function (feature, layer) {
       layer.bindPopup(feature.properties.title); // Shows "title" info on popup
     },
-    pointToLayer: function(feature, latlng){
+    pointToLayer: function (feature, latlng) {
       return L.circleMarker(latlng, {
         radius: chooseRadius(feature.properties.mag), // pulls mag (magnitude) data to determine marker size
         fillColor: chooseColor(feature.geometry.coordinates[2]), // pulls depth data to determine marker color
@@ -70,11 +69,27 @@ d3.json(queryUrl).then(function(data) {
     }
   }).addTo(myMap);
 
-// ***Create legend showing the depth and their corresponding color **** NEED HELP WITH THIS
-L.control.attribution({
-  position: "bottomright", 
-  prefix: "<h3>Note:</h3><h4>Size of marker increases with increasing earthquake magnitude<br>Color of marker darkens with increasing earthquake depth</h4>"
-
-}).addTo(myMap);
-
+  // Create legend 
+  // First explain relation of size and color to magnitude and depth
+  L.control.attribution({
+    position: "bottomright", 
+    prefix: "<b><li>Size of marker increases with increasing earthquake magnitude</li><li>Color of marker darkens with increasing earthquake depth</li></b>"
+  }).addTo(myMap);
+  // Next provide colors and corresponding depths
+  var legend = L.control({ position: "topright" });
+  legend.onAdd = function(legend) {
+    var div = L.DomUtil.create("div", "info legend");
+    var limits = [0, 51, 101, 151, 201, 251, 301];
+    var limit_descr = ['0-50km', '51-100km', '101-150km', '151-200km', '201-250km', '251-300km', '300+km'];
+    var labels = [];
+    // With the forEach function, map colors from chooseColor function to depths in limit_desc array
+    limits.forEach(function (limit, index) {
+      var color = chooseColor(limit);
+      labels.push("<li class=\"legend-div\" style=\"background-color: " + color + "\">" + limit_descr[index] + "</li>");
+    })
+    div.innerHTML = "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+  legend.addTo(myMap);
+  
 });
